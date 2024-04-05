6413c04aae10bfbc30734d52215f80f9d955da0b
Brea Bailey <156851469+baileybt@users.noreply.github.com>
2024-04-05 15:42:35 -0400

    Update HomePage.vue
    Joined all text into one paragraph

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -4,13 +4,9 @@
             <h1 class="page-title"><b>OSoMe Mastodon Search</b></h1>
             <div class="col-12">
                 <div class="paragraph-text">
-                    <p>OSoMe Mastodon Search provides an interface to various functionalities, allowing users to retrieve statuses, explore accounts, access hashtag metadata, discover instances, and access comprehensive documentation. This empowers researchers to experiment with Mastodon data, enabling in-depth exploration and analysis for valuable insights.</p>
+                    <p>OSoMe Mastodon Search provides an interface to various functionalities, allowing users to retrieve statuses, explore accounts, access hashtag metadata, discover instances, and access comprehensive documentation. This empowers researchers to experiment with Mastodon data, enabling in-depth exploration and analysis for valuable insights. As Mastodon is a federated social network, the OSoMe Mastodon Search interface allows users to search for keywords and hashtags <b>across multiple Mastodon instances</b>.</p>
                 </div>
             </div>
-            <div class="col-12">
-                <div class="paragraph-text">
-                    <p>As Mastodon is a federated social network, the OSoMe Mastodon Search interface allows users to search for keywords and hashtags <b>across multiple Mastodon instances</b>.</p>
-                </div>
             </div>
             <div class="col-12 d-flex justify-content-center">
                 <img src="../assets/homepage.jpg" alt="Italian Trulli" class="img-fluid" style="max-width: 70%; height: 700px;">
